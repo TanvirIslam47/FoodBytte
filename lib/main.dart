681392d4b2447a2dfafd3210ed2b898458b3d132
TanvirIslam47 <rockingtanvir@gmail.com>
2022-12-17 00:40:32 +0600

    debug mode banner set false

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: Constants.appName,
       theme: Constants.lightTheme,
       darkTheme: Constants.darkTheme,
       home: MainScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
